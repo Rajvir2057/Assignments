d341ba74149d2107043441ce4c1ec2aef1ffa8a9
beenp <arwensonwidjaja36@gmail.com>
2025-10-15 16:10:29 -0700

    added data setter&getters

--- Assignment1/Node.cpp
+++ Assignment1/Node.cpp
@@ -1,11 +1,18 @@
 #include "Node.h"
 
-    //constructor
-    Node :: Node(int d, Node* n) : data(d), next(n){}
-    
-    Node* Node::getNext() const {
-        return next;
-    }
-    void Node::setNext(Node* n) {
-        next = n;
-    }
+//constructor
+Node :: Node(int d, Node* n) : data(d), next(n){}
+
+Node* Node :: getNext() const {
+    return next;
+}
+void Node :: setNext(Node* n) {
+    next = n;
+}
+
+int Node :: getData() const{
+    return data;
+}
+void Node :: setData(const int& i){
+    data = i;
+}
--- Assignment1/Node.h
+++ Assignment1/Node.h
@@ -1,14 +1,18 @@
 #ifndef _NODE
 #define _NODE
 struct Node{
+    private:
+        int data;
+        Node* next;
 
-    int data;
-    Node* next;
+    public:
+        Node(int d, Node* n = nullptr); // ctor declared here.
 
-    Node(int d, Node* n = nullptr); // ctor declared here.
+        Node* getNext() const;
+        void setNext(Node* n);
 
-    Node* getNext() const;
-    void setNext(Node*);
+        int getData() const;
+        void setData(const int& i);
 };
 
 #include "Node.cpp"
