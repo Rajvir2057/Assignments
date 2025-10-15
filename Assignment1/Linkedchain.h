c8d53e8bff2dff6453952501e308df3c6b9fb957
beenp <arwensonwidjaja36@gmail.com>
2025-10-15 16:09:29 -0700

    finished

--- Assignment1/Linkedchain.h
+++ Assignment1/Linkedchain.h
@@ -1,23 +1,23 @@
 #ifndef _LINKEDCHAIN
 #define _LINKEDCHAIN
+
 #include "Node.h"
 
 class LinkedChain {
 
-    public :
-        void push_back ( int x ) ; // TODO
-        void pop_front () ; // TODO ( assume non - empty when called interanlly0
-        int front () const ; // TODO ( assume non - empty )
-        bool empty () const ; // TODO
-        ~ LinkedChain () ; // TODO ( clean up nodes )
+public :
+    void push_back (const int& x ) ; // TODO
+    void pop_front () ; // TODO ( assume non - empty when called interanlly0
+    int front () const ; // TODO ( assume non - empty )
+    bool empty () const ; // TODO
+    ~ LinkedChain () ; // TODO ( clean up nodes )
 
 
-    private :
-        Node * head_ = nullptr ;
-        Node * tail_ = nullptr;
+private :
+    Node * head_ = nullptr ;
+    Node * tail_ = nullptr;
 
 };
 
 
-#include "Linkedchain.cpp"
 #endif
