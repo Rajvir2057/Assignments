399915e45b231b0726d246bb3f044cdbc2ccb616
beenp <arwensonwidjaja36@gmail.com>
2025-10-18 17:00:58 -0700

    made member functions public and const

--- Assignment1/Student.h
+++ Assignment1/Student.h
@@ -5,7 +5,7 @@
 #include <string>
 
 class Student {
-
+    public:
     // getter and setters
     Student();
     Student (const int& id, const std :: string& name, const std :: string& completed);
@@ -17,7 +17,7 @@ class Student {
 
     // course completion
     void addCompleted(const std :: string& c);
-    bool hasCompleted (const std :: string& c);
+    bool hasCompleted (const std :: string& c) const;
     std :: vector<std :: string> completedCourses() const;
 
     // add meat to all the above.
