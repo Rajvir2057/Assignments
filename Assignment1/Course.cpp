24b26bce750820aca48958c518944031028ca17a
beenp <arwensonwidjaja36@gmail.com>
2025-10-18 17:01:38 -0700

    finished

--- Assignment1/Course.cpp
+++ Assignment1/Course.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <string>
+#include "Course.h"
 
-class Course{
-    
-    public:
-    //getters and setters
-    std :: string code() const;
-    void setCode(const std :: string& c);
+std :: string Course::code() const{
+    return code_;
+}
+void Course::setCode(const std :: string& c){
+    code_ = c;
+} 
 
-    void addPrereq(const std :: string& c);
-    std :: vector<std :: string> prereqs() const;
+void Course::addPrereq(const std :: string& c){
+    prereqs_.push_back(c);
+}
+std :: vector<std :: string> Course::prereqs() const{
+    return prereqs_;
+}
 
-    void setTitle(const std :: string& t);
-    std :: string title() const;
-
-    private:
-
-    std :: string code_;
-    std :: string title_;
-    std :: vector<std :: string> prereqs_;
-};
+void Course::setTitle(const std :: string& t){
+    title_ = t;
+}
+std :: string Course::title() const{
+    return title_;
+}
---
+++ Assignment1/Course.h
@@ -0,0 +1,29 @@
+#ifndef COURSE_
+#define COURSE_
+
+#include <vector>
+#include <string>
+
+class Course{
+    
+    public:
+    //getters and setters
+    std :: string code() const;
+    void setCode(const std :: string& c);
+    
+
+    void addPrereq(const std :: string& c);
+    std :: vector<std :: string> prereqs() const;
+
+
+    void setTitle(const std :: string& t);
+    std :: string title() const;
+
+    private:
+
+    std :: string code_;
+    std :: string title_;
+    std :: vector<std :: string> prereqs_;
+};
+#include "Course.cpp"
+#endif  
